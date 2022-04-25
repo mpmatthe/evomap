04840f773ea516382d3f57106e61b4c537b2c2c3
Maximilian Matthe <matthe@wiwi.uni-frankfurt.de>
2022-04-25 20:07:51 +0200

    fix: Fix commit

--- src/marketmaps/datasets.py
+++ src/marketmaps/datasets.py
@@ -6,6 +6,51 @@ Author: Maximilian Matthe <matthe@wiwi.uni-frankfurt.de>
 from importlib import resources
 import numpy as np
 import os
+import pandas as pd
+
+def load_tnic_snapshot():
+    """ Load example data. Original data comes from 
+    https://hobergphillips.tuck.dartmouth.edu/.  
+
+    Returns
+    -------
+    dict
+        Dictionary containing the similarity matrix, firm labels and 
+        cluster assignments (based on community detection).
+    """
+    with resources.path("marketmaps.data.tnic_snapshot", "sim_mat.npy") as f:
+        sim_mat = np.load(f)
+
+    with resources.path("marketmaps.data.tnic_snapshot", "cluster.npy") as f:
+        cluster = np.load(f)
+
+    with resources.path("marketmaps.data.tnic_snapshot", "labels.npy") as f:
+        labels = np.load(f)
+
+    TNIC_testdata = {'matrix': sim_mat, 'labels': labels, 'cluster': cluster}
+    return TNIC_testdata
+
+def load_tnic_snapshot_small():
+    """ Load example data. Original data comes from 
+    https://hobergphillips.tuck.dartmouth.edu/.  
+
+    Returns
+    -------
+    dict
+        Dictionary containing the similarity matrix, firm labels and 
+        cluster assignments (based on community detection).
+    """
+    with resources.path("marketmaps.data.tnic_snapshot_small", "sim_mat.npy") as f:
+        sim_mat = np.load(f)
+
+    with resources.path("marketmaps.data.tnic_snapshot_small", "cluster.npy") as f:
+        cluster = np.load(f)
+
+    with resources.path("marketmaps.data.tnic_snapshot_small", "labels.npy") as f:
+        labels = np.load(f)
+
+    TNIC_testdata = {'matrix': sim_mat, 'labels': labels, 'cluster': cluster}
+    return TNIC_testdata
 
 def load_tnic_sample():
     """ Load example data. Original data comes from 
@@ -17,17 +62,11 @@ def load_tnic_sample():
         Dictionary containing the similarity matrix, firm labels and 
         cluster assignments (based on community detection).
     """
-    with resources.path("marketmaps.data.tnic_sample", "sim_mat.npy") as f:
-        sim_mat = np.load(f)
+    with resources.path("marketmaps.data.tnic_sample", "tnic_sample.csv") as f:
+        df_tnic = pd.read_csv(f)
 
-    with resources.path("marketmaps.data.tnic_sample", "cluster.npy") as f:
-        cluster = np.load(f)
+    return df_tnic
 
-    with resources.path("marketmaps.data.tnic_sample", "labels.npy") as f:
-        labels = np.load(f)
-
-    TNIC_testdata = {'matrix': sim_mat, 'labels': labels, 'cluster': cluster}
-    return TNIC_testdata
 
 def load_tnic_sample_small():
     """ Load example data. Original data comes from 
@@ -39,14 +78,7 @@ def load_tnic_sample_small():
         Dictionary containing the similarity matrix, firm labels and 
         cluster assignments (based on community detection).
     """
-    with resources.path("marketmaps.data.tnic_sample_small", "sim_mat.npy") as f:
-        sim_mat = np.load(f)
+    with resources.path("marketmaps.data.tnic_sample_small", "tnic_sample_small.csv") as f:
+        df_tnic = pd.read_csv(f)
 
-    with resources.path("marketmaps.data.tnic_sample_small", "cluster.npy") as f:
-        cluster = np.load(f)
-
-    with resources.path("marketmaps.data.tnic_sample_small", "labels.npy") as f:
-        labels = np.load(f)
-
-    TNIC_testdata = {'matrix': sim_mat, 'labels': labels, 'cluster': cluster}
-    return TNIC_testdata
+    return df_tnic
