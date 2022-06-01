232b9e8cfd3b1445d1867f2de36789a0f6ee8482
Maximilian Matthe <matthe@wiwi.uni-frankfurt.de>
2022-06-01 13:43:30 +0200

    feat: Python 3.7 support

--- src/evomap/__init__.py
+++ src/evomap/__init__.py
@@ -1,3 +1,9 @@
 # read version from installed package
-from importlib.metadata import version
+import sys
+if sys.version.startswith('3.7'):
+    from importlib_metadata import version
+elif sys.version.startswith('3.8'):
+    from importlib_metadata import version
+else:
+    from importlib.metadata import version
 __version__ = version("evomap")
