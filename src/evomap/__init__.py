04506783372fe6ef7783a6c206e0d9e88269ef46
Alexey Pechnikov <pechnikov@mobigroup.ru>
2024-09-28 20:20:05 +0700

    Remove directives for Python 3.7 and 3.8 (unsupported in datasets.py and across codebase)

--- src/evomap/__init__.py
+++ src/evomap/__init__.py
@@ -1,9 +1,3 @@
 # read version from installed package
-import sys
-if sys.version.startswith('3.7'):
-    from importlib_metadata import version
-elif sys.version.startswith('3.8'):
-    from importlib_metadata import version
-else:
-    from importlib.metadata import version
+from importlib.metadata import version
 __version__ = version("evomap")
